f1afb38c3baf5f7071934be1112b5d23d5ea90f6
rob rod <rrobrrod14@hotmail.com>
2021-12-20 22:42:09 -0500

    Fixed the missing GPIO in webserver and added color to Camera

--- Camera.py
+++ Camera.py
@@ -1,20 +1,30 @@
 import requests
-
 from pynput import keyboard
 
-camera1_ip = '10.1.10.93' #'172.16.105.131'
-camera2_ip = '10.1.10.138'
+GREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+END = '\033[0m'
+BOLD = '\x1b[1m'
+RESET = '\x1b[21m'
+
+camera1_ip =  '10.1.10.93' #'192.168.1.54'
+camera2_ip =  '10.1.10.138' #'192.168.1.67'
 cmd_to_execute = 'flask run'
 camera1 = 'off'
 camera2 = 'off'
 
 
 def check_server_ready(ip_address):
-    r = requests.get(f'http://{ip_address}:5000')
-    if r.status_code != 200:
-       return False
-    else:
-       return True
+    try:
+        r = requests.get(f'http://{ip_address}:5000')
+        if r.status_code == 200:
+            return True
+    except requests.exceptions.ConnectionError  as ce:
+        print(f'{FAIL}Network problems;{END} {ce}.')
+    except requests.exceptions.Timeout as to:
+        print(f'{FAIL}Timeout Issues;{END} {to}.')
+
 
 def turn_camera_on(ip_address):
     r = requests.get(f'http://{ip_address}:5000/api/v1/camera?Turn=on')
@@ -37,27 +47,27 @@ def on_press(key):
             camera1 = turn_camera_on(camera1_ip)
             if camera2 == 'on':
                 camera2 = turn_camera_off(camera2_ip)
-            #print(f'\nCamera1 is {camera1}. Camera2 is {camera2}. First print')
             return True
         elif key.char == "2" and camera2 != "on":
             camera2 = turn_camera_on(camera2_ip)
             if camera1 == 'on':
                 camera1 = turn_camera_off(camera1_ip)
-            #print(f'\nCamera1 is {camera1}. Camera2 is {camera2}. Second print')
             return True
         else:
             if camera1 == 'on' and key.char != "1":
                 camera1 = turn_camera_off(camera1_ip)
             elif camera2 == 'on' and key.char != "2":
                 camera2 = turn_camera_off(camera2_ip)
-            #print(f'\nCamera1 is {camera1}. Camera2 is {camera2}. Third print {key.char} {type(key.char)}')
     except AttributeError as ex:
         print(ex)
+        pass
 
 
 def on_release(key):
-    if key == keyboard.Key.esc:
-        return False
+    if key.char == '0':
+        print(f"{WARNING}Shutting down...{END}")
+        exit()
+
 
 
 def wait_for_user_input():
@@ -69,12 +79,21 @@ def wait_for_user_input():
 print(f'Running Camera switcher')
 print(f'Checking if Camera 1 is up and running')
 if check_server_ready(camera1_ip):
-   print(f"Camera 1 server is ready!")
+   print(f"{GREEN}Camera 1 server is ready!{END}")
+   camera1_ready = True
 else:
-   print(f"Camera 1 server is not ready")
+   print(f"{WARNING}Camera 1 server is not ready. Please make sure the server is on or give it more time.{END}")
+   camera1_ready = False
 print(f'Checking if Camera 2 is up and running')
 if check_server_ready(camera2_ip):
-   print(f"Camera 2 server if ready!")
+   print(f"{GREEN}Camera 2 server if ready!{END}")
+   camera2_ready = True
 else:
-   print(f"Camera 2 server is not ready!")
+   print(f"{WARNING}Camera 2 server is not ready. Please make sure the server is on or give it more time.{END}")
+   camera2_ready = False
+if camera1_ready == False or camera2_ready == False:
+    print(f"{FAIL}Shutting down. So you can fix the servers.{END}")
+    exit()
+else:
+    print(f'Press {FAIL}{BOLD}"0"{RESET}{END} to close the program')
 wait_for_user_input()
--- Webserver/webserver.py
+++ Webserver/webserver.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from flask import request
 import time
+import RPi.GPIO as GPIO
+
+i = 0
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+Output_PIN = 23
+
+GPIO.setup(Output_PIN, GPIO.OUT)
+print('Starting up the PIR Module')
+print('Ready')
 
 app = Flask(__name__)
 
 def turn_off():
-    print(f'sending power once')
-    print(f'sending power twice')
-    print(f'sending power thrice')
+    GPIO.output(Output_PIN, GPIO.LOW)
+    print(f'sending power')
 
 def turn_on():
-    print(f'sending power once')
+    GPIO.output(Output_PIN, GPIO.HIGH)
+    print(f'sending power')
 
 @app.route("/")
 def hello_world():
@@ -26,15 +36,6 @@ def camera_one():
 
     return "<p>You sent command to camera 1 </p>"
 
-@app.route("/api/v1/2")
-def camera_two():
-    turn = request.args.get('Turn')
-    if turn == 'off':
-        turn_off()
-    elif turn == 'on':
-        turn_on()
-
-    return "<p>You sent command to cammera 2 </p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
